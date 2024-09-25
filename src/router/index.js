b90988db01107db35c03cd5715944826c6300462
KarenRamos2 <89101028+KarenRamos2@users.noreply.github.com>
2024-09-25 17:08:14 -0500

    feat: add register payment

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import GroupOverview from '../components/Groups/views/GroupOverview.vue';
 import GroupDetailView from '../components/Groups/views/GroupDetailView.vue';
 import GroupCreateView from '../components/Groups/views/GroupCreateView.vue';
 import StartPageView from '../components/views/StartPageView.vue';
+import registerPayment from '@/contexts/payment/components/register-payment.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: StartPageView },
@@ -13,7 +14,8 @@ const routes = [
     component: GroupDetailView,
     props: true
   },
-  { path: '/grupo/crear-grupo', name: 'GroupCreate', component: GroupCreateView }
+  { path: '/grupo/crear-grupo', name: 'GroupCreate', component: GroupCreateView },
+  { path: '/registrar-pago', name: 'RegisterPayment', component: registerPayment }
 ]
 
 const router = createRouter({
