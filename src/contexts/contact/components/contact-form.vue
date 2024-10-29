6b1d790ceab696bc203ad071ecc65a89115f16bf
cristianipa7 <cristianir007@gmail.com>
2024-10-28 20:39:01 -0500

    feat(naming): change variable name

---
+++ src/contexts/contact/components/contact-form.vue
@@ -0,0 +1,109 @@
+<script setup>
+import ContactName from '@/contexts/contact/components/contact-name.vue'
+import ContactEmail from '@/contexts/contact/components/contact-email.vue'
+import FormActions from '@/contexts/contact/components/form-actions.vue'
+import { Contact } from '../models/contact-entity.js'
+import { ContactApiService } from '@/contexts/contact/services/contact-api.js'
+import { useRoute } from 'vue-router'
+import { onMounted, reactive, ref } from 'vue'
+import router from '@/router/index.js'
+
+const routes = useRoute()
+const emit = defineEmits(['cancel'])
+const form = reactive(new Contact())
+const contactApiService = new ContactApiService()
+const id = ref(routes.params.id || '')
+
+const onSubmit = () => {
+  if (id.value !== '') {
+    contactApiService
+      .update(id.value, form)
+      .then((response) => {
+        if (response.status == 200) {
+          router.push('/contact')
+        }
+        emit('save')
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+    return
+  }
+
+  const { id: omitId, ...newContactData } = form
+    contactApiService
+      .save(newContactData)
+      .then((response) => {
+        if (response.status == 201) {
+          router.push('/contact')
+        }
+        emit('save')
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }
+
+  onMounted(() => {
+    if (id.value) {
+      contactApiService.get(id.value).then((response) => {
+        const contactData = response.data
+
+        form.name = contactData.name
+        form.email = contactData.email
+        form.id = contactData.id
+      })
+    }
+  })
+
+  const onCancel = () => {
+    form.name = ''
+    form.email = ''
+    form.id = 0
+
+    emit('cancel')
+}
+</script>
+
+<template>
+  <div class="contact-form-container">
+    <form @submit.prevent="onSubmit" class="form-container">
+      <h1>Nuevo contacto</h1>
+      <div class="form-group">
+        <contact-name v-model="form.name"></contact-name>
+      </div>
+      <div class="form-group">
+        <contact-email v-model="form.email"></contact-email>
+      </div>
+      <div class="form-actions">
+        <form-actions @save="onSubmit" @cancel="onCancel"></form-actions>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.contact-form-container {
+}
+.form-container {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+.form-actions {
+  justify-content: center;
+  display: flex;
+  padding-top: 40px;
+}
+
+h1{
+  text-align: center;
+  color: #B1375B;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import GroupOverview from '@/contexts/groups/pages/group-overview.page.vue';
 import GroupDetailView from '@/contexts/groups/pages/group-detail.page.vue';
 import GroupCreateView from '@/contexts/groups/pages/create-group.page.vue';
 import StartPageView from '@/contexts/shared/pages/the-home.page.vue';
-import ContactoForm from '@/contexts/contacto/components/contacto-form.vue'
-import ContactoManagement from '@/contexts/contacto/pages/contacto-management.vue'
+import ContactForm from '@/contexts/contact/components/contact-form.vue'
+import ContactManagement from '@/contexts/contact/pages/contact-management.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: StartPageView },
@@ -16,9 +16,9 @@ const routes = [
     props: true
   },
   { path: '/group/create-group', name: 'GroupCreate', component: GroupCreateView },
-  { path: '/contacto', component: ContactoManagement },
-  { path: '/create-contacto', component: ContactoForm, name: 'createContacto' },
-  { path: '/update-contacto/:id', component: ContactoForm, name: 'updateContacto' }
+  { path: '/contacts', component: ContactManagement },
+  { path: '/create-contact', component: ContactForm, name: 'createContact' },
+  { path: '/update-contact/:id', component: ContactForm, name: 'updateContact' }
 ]
 
 const router = createRouter({
